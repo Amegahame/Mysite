327df21a5ccc54422114bebdbf89bde2cdeb48a7
diogo patrick <diogo_p@hotmail.com.br>
2025-05-27 00:05:06 -0300

    Alterações temporárias em urls e views

--- meu_projeto/portfolio/urls.py
+++ meu_projeto/portfolio/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from . import views 
+from . import views
 
 urlpatterns = [
-    path('', views.alguma_view, name='home'),   # ✅ rota principal
-    path('post/', views.post_view, name='post_view'),  # ✅ rota /post/
+    path('post/', views.post_view, name='post_view'),
 ]
